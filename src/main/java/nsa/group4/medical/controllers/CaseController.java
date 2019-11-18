8cefe49d94b33a8fe36e4c3acfd84ebbe9240eed
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-11-18 20:13:25 +0000

    added logger to case controller, added validation for case form input

--- src/main/java/nsa/group4/medical/controllers/CaseController.java
+++ src/main/java/nsa/group4/medical/controllers/CaseController.java
@@ -2,19 +2,43 @@ package nsa.group4.medical.controllers;
 
 
 import nsa.group4.medical.domains.CaseModel;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 @Controller
 public class CaseController {
 
+    static final Logger LOG = LoggerFactory.getLogger(CaseController.class);
 
     @RequestMapping(path="/createNewCase", method = RequestMethod.GET)
     public String createNewCase(Model model){
 
         model.addAttribute("caseKey", new CaseModel());
         return "newCase";
+    }
+
+    @RequestMapping(path="/caseDetails", method = RequestMethod.POST)
+    public String caseAdded(@ModelAttribute("caseKey") @Valid CaseModel caseModel,
+                            BindingResult bindingResult,
+                            Model model){
+
+        LOG.debug(caseModel.toString());
+
+        if (bindingResult.hasErrors()){
+            LOG.debug(bindingResult.toString());
+            return "newCase";
+        }
+
+
+
+        return "index";
     }
 }
