e41521c2a92a06019f80a380a5e30bb6e2c1d77e
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-11-18 19:44:09 +0000

    added request mapping to create new case that returns the new case page

--- src/main/java/nsa/group4/medical/controllers/CaseController.java
+++ src/main/java/nsa/group4/medical/controllers/CaseController.java
@@ -1,4 +1,20 @@
 package nsa.group4.medical.controllers;
 
+
+import nsa.group4.medical.domains.CaseModel;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
 public class CaseController {
+
+
+    @RequestMapping(path="/createNewCase", method = RequestMethod.GET)
+    public String createNewCase(Model model){
+
+        model.addAttribute("caseKey", new CaseModel());
+        return "newCase";
+    }
 }
