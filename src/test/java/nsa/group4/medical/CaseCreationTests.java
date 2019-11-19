cf11adff43d5fe9219b601b0afbe7448207470fd
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-11-19 14:58:09 +0000

    added blank test class for case creation

---
+++ src/test/java/nsa/group4/medical/CaseCreationTests.java
@@ -0,0 +1,12 @@
+package nsa.group4.medical;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.transaction.Transactional;
+
+@RunWith(SpringRunner.class)
+@Transactional
+public class CaseCreationTests {
+    
+}
