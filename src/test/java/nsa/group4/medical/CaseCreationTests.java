198bf97f70a1623c9bf6a0cf1eaaf6b1a12262a0
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-11-19 15:10:36 +0000

    added context loads test for case controller

--- src/test/java/nsa/group4/medical/CaseCreationTests.java
+++ src/test/java/nsa/group4/medical/CaseCreationTests.java
@@ -1,12 +1,25 @@
 package nsa.group4.medical;
 
+import nsa.group4.medical.controllers.CaseController;
+import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.transaction.Transactional;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @Transactional
+@SpringBootTest
 public class CaseCreationTests {
-    
+    @Autowired
+    private CaseController caseController;
+
+    @Test
+    public void contextLoads() throws Exception{
+        assertThat(caseController).isNotNull();
+    }
 }
