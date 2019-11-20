c3c572c3a7086bdc40de8270c3c749cbfada55b1
Jake Morris <Jakemorrishello@gmail.com>
2019-11-20 21:50:35 +0000

    fixed merge conflict, diagnosis entity now uses category object instead of an id

--- src/main/java/nsa/group4/medical/controllers/DiagnosesController.java
+++ src/main/java/nsa/group4/medical/controllers/DiagnosesController.java
@@ -4,14 +4,13 @@ import nsa.group4.medical.data.CategoriesRepositoryJPA;
 import nsa.group4.medical.data.DiagnosisRepositoryJPA;
 import nsa.group4.medical.domains.Categories;
 import nsa.group4.medical.domains.Diagnosis;
+import nsa.group4.medical.web.CaseForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,11 +27,29 @@ public class DiagnosesController {
         this.categoriesRepositoryJPA = categoriesRepositoryJPA;
     }
 
-    @GetMapping("/diagnoses/{index}")
-    public String getDiagnoses(Model model, @PathVariable final Long index) {
+
+    @GetMapping("dia/{index}")
+    public String getDiagnoses(Model model,
+                               @PathVariable final Long index) {
         Optional<Categories> categoriesOptional = categoriesRepositoryJPA.findById(index);
+
         List<Diagnosis> diagnoses = diagnosisRepositoryJPA.findByCategories(categoriesOptional.get());
+        model.addAttribute("diagnosisKey", new Diagnosis());
         model.addAttribute("diagnoses", diagnoses);
         return "home";
+    }
+
+    @PostMapping("dia/{index}")
+    public String newDiagnosis(@PathVariable final Long index,
+                               @ModelAttribute("diagnosisKey") Diagnosis diagnosis,
+                               Model model) {
+        Categories categories = categoriesRepositoryJPA.findById(index).get();
+        diagnosis.setCategories(categories);
+        diagnosisRepositoryJPA.save(diagnosis);
+        List<Diagnosis> diagnoses = diagnosisRepositoryJPA.findByCategories(categories);
+        model.addAttribute("diagnosisKey", new Diagnosis());
+        model.addAttribute("diagnoses", diagnoses);
+        return "home";
+
     }
 }
