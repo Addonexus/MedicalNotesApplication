ad3f2d349e7a1ae5d14308de0e9cb4abe59735f0
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-11-25 19:27:40 +0000

    added postmapping for saveDiagnosisInformation in diagnosescontroller which validates form and saves diagnosis info in database using jdbc

--- src/main/java/nsa/group4/medical/controllers/DiagnosesController.java
+++ src/main/java/nsa/group4/medical/controllers/DiagnosesController.java
@@ -2,14 +2,20 @@ package nsa.group4.medical.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import nsa.group4.medical.data.CategoriesRepositoryJPA;
+import nsa.group4.medical.data.DiagnosisInformationRepositoryJDBC;
 import nsa.group4.medical.data.DiagnosisRepositoryJPA;
 import nsa.group4.medical.domains.Categories;
 import nsa.group4.medical.domains.Diagnosis;
 import nsa.group4.medical.service.DiagnosisRepositoryInterface;
+import nsa.group4.medical.service.events.DiagnosisInformationAdded;
 import nsa.group4.medical.web.CaseForm;
+import nsa.group4.medical.web.DiagnosisInformationForm;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -22,12 +28,17 @@ public class DiagnosesController {
 
     private DiagnosisRepositoryInterface diagnosisRepository;
     private CategoriesRepositoryJPA categoriesRepositoryJPA;
+    private DiagnosisInformationRepositoryJDBC diagnosisInformationRepositoryJDBC;
+
+    static final Logger LOG = LoggerFactory.getLogger(DiagnosesController.class);
 
     @Autowired
     public DiagnosesController(DiagnosisRepositoryInterface diagnosisRepository,
-                               CategoriesRepositoryJPA categoriesRepositoryJPA) {
+                               CategoriesRepositoryJPA categoriesRepositoryJPA,
+                               DiagnosisInformationRepositoryJDBC diagnosisInformationRepositoryJDBC) {
         this.diagnosisRepository = diagnosisRepository;
         this.categoriesRepositoryJPA = categoriesRepositoryJPA;
+        this.diagnosisInformationRepositoryJDBC = diagnosisInformationRepositoryJDBC;
     }
 
 
@@ -42,6 +53,29 @@ public class DiagnosesController {
 //        return "home";
 //    }
 
+    @PostMapping(path ="/category/{categoryIndex}/diagnosis/{diagnosisIndex}/addDiagnosisInfo")
+    public String saveDiagnosisInformation(@PathVariable(name="categoryIndex") Long categoryID,
+                                           @PathVariable(name="diagnosisIndex") Long diagnosisID,
+                                           @ModelAttribute("diagnosisInfoKey") @Valid DiagnosisInformationForm diagnosisInformationForm,
+                                           BindingResult bindingResult,
+                                           Model model) {
+
+        if (bindingResult.hasErrors()){
+            LOG.debug(bindingResult.toString());
+            return "diagnosisInformation";
+        }
+
+        DiagnosisInformationAdded diagnosisInformationAdded = new DiagnosisInformationAdded(
+                diagnosisID,
+                diagnosisInformationForm.getKey(),
+                diagnosisInformationForm.getValue()
+        );
+
+        diagnosisInformationRepositoryJDBC.saveDiagnosisInformation(diagnosisInformationAdded);
+
+        return "home";
+    }
+
 
     @GetMapping(path ="/category/{categoryIndex}/diagnosis/{diagnosisIndex}/addDiagnosisInfo")
     public String addDiagnosisInformation(@PathVariable(name="categoryIndex") Long categoryID,
@@ -53,6 +87,8 @@ public class DiagnosesController {
         if (!category.isPresent()){
             return "404";
         }
+
+        model.addAttribute("diagnosisInfoKey", new DiagnosisInformationForm());
 
         return "diagnosisInformation";
     }
