601ee91cf765247feb2993a63686014a829bbe2a
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-11-25 16:22:53 +0000

    User domain class created

---
+++ src/main/java/nsa/group4/medical/domains/User.java
@@ -0,0 +1,38 @@
+package nsa.group4.medical.domains;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.util.Set;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name= "user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    private String password;
+
+
+    @ManyToMany
+    private Set<Role> roles;
+
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
