c6a37401a7ad4e8173aba9277e2bda17086a25b2
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-11-26 20:04:56 +0000

    user service implementation created and added a method in application to by pass error but will have to change in the future

--- src/main/java/nsa/group4/medical/CaseADayApplication.java
+++ src/main/java/nsa/group4/medical/CaseADayApplication.java
@@ -3,8 +3,10 @@ package nsa.group4.medical;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @ComponentScan({"nsa.group4"})
@@ -16,4 +18,9 @@ public class CaseADayApplication {
         SpringApplication.run(CaseADayApplication.class, args);
     }
 
+    //might need to remove from here
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
