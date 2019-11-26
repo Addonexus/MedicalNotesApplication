4ba7424dd9a7134632aeae50ce303a554d9af972
Anson Gurung <GurungA4@cardiff.ac.uk>
2019-11-26 12:22:39 +0000

    Created FieldErrors Class to store the fieldname and the errormessage that will be passed back as a List of this object, changed ResponseBody class to this this object rather than ReturnedData

--- src/main/java/nsa/group4/medical/controllers/api/DiagnosisFieldsError.java
+++ src/main/java/nsa/group4/medical/controllers/api/DiagnosisFieldsError.java
@@ -4,11 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.util.List;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AjaxResponseBody {
-    String status;
-    List<DiagnosisFieldsError> result;
+public class DiagnosisFieldsError {
+    private String fieldName;
+    private String errorMessage;
 }
