f26fde20d0a7f3656c129cd14fd24adad8946234
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-11-26 14:07:34 +0000

    completed diagnosis information rowmapper class

--- src/main/java/nsa/group4/medical/domains/rowmappers/DiagnosisInformationRowmapper.java
+++ src/main/java/nsa/group4/medical/domains/rowmappers/DiagnosisInformationRowmapper.java
@@ -1,4 +1,20 @@
 package nsa.group4.medical.domains.rowmappers;
 
-public class DiagnosisInformationRowmapper {
+import nsa.group4.medical.domains.DiagnosisInformation;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DiagnosisInformationRowmapper implements RowMapper<DiagnosisInformation> {
+
+    @Override
+    public DiagnosisInformation mapRow(ResultSet resultSet, int rowNum) throws SQLException{
+        DiagnosisInformation diagnosisInformation = new DiagnosisInformation();
+        diagnosisInformation.setId(resultSet.getLong("diagnosis_id"));
+        diagnosisInformation.setKey(resultSet.getString("key"));
+        diagnosisInformation.setValue(resultSet.getString("value"));
+
+        return diagnosisInformation;
+    }
 }
