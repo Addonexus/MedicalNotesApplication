bbfbd377c82934dfaf3e36018d28f015810c7300
Luke Arnold <ArnoldL@cardiff.ac.uk>
2019-12-03 18:41:18 +0000

    created notification controller, added mapping to get all notifications from database

---
+++ src/main/java/nsa/group4/medical/controllers/NotificationController.java
@@ -0,0 +1,28 @@
+package nsa.group4.medical.controllers;
+
+
+import lombok.extern.slf4j.Slf4j;
+import nsa.group4.medical.data.NotificationRepositoryJDBC;
+import nsa.group4.medical.domains.Notification;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Slf4j
+@Controller
+public class NotificationController {
+
+    private NotificationRepositoryJDBC notificationRepositoryJDBC;
+
+    @Autowired
+    public NotificationController(NotificationRepositoryJDBC notificationRepositoryJDBC) {this.notificationRepositoryJDBC = notificationRepositoryJDBC;}
+
+    @GetMapping(value="/notifications")
+    public String getNotifications(Model model){
+
+        List<Notification> notificationList = notificationRepositoryJDBC.getAllNotifications();
+    }
+}
