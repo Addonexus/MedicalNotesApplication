fc262e6382997ffde238515e390c7e43a828bba7
Jake Morris <Jakemorrishello@gmail.com>
2019-12-03 18:56:51 +0000

    now makes a request to the api controller

--- src/main/resources/static/sitewide.js
+++ src/main/resources/static/sitewide.js
@@ -14,4 +14,22 @@ console.log(document.getElementsByClassName("calendar-form"));
 $("#calendar-form").submit(function(e) {
   e.preventDefault();
   var url = "/api/getCasesByDate/";
+
+  var formData = {
+    name: document.getElementById("date").value
+  };
+
+  $.ajax({
+    contentType: "application/json; charset=utf-8",
+    dataType: "json",
+    type: "POST",
+    url: url,
+    data: JSON.stringify(formData),
+    success: function(json) {
+      console.log(json);
+    },
+    error: function(json) {
+      alert("error!");
+    }
+  });
 });
