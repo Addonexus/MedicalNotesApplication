2b1f7b08dc32de7a18865c4ba520df156aa3713c
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-03 20:13:16 +0000

    modified user table again for registration because were using a new method

--- src/main/java/nsa/group4/medical/domains/User.java
+++ src/main/java/nsa/group4/medical/domains/User.java
@@ -7,33 +7,51 @@ import lombok.NoArgsConstructor;
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name="auth_user")
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auth_user_id")
     private Long id;
 
-    @Column(nullable = false, unique = true)
+    @Column(name = "first_name")
     @NotNull
     @NotEmpty
-    private String username;
+    private String firstName;
 
+    @Column(name = "last_name")
+    @NotNull
+    @NotEmpty
+    private String lastName;
+
+    @Column(name = "email")
+    @NotNull
+    @NotEmpty
+    private String email;
+
+    @Column(name = "password")
     @NotNull
     @NotEmpty
     private String password;
 
+    @Column(name = "status")
+    @NotNull
+    @NotEmpty
+    private String status;
 
-    @Override
-    public String toString() {
-        return "User{" +
-                "id=" + id +
-                ", username='" + username + '\'' +
-                ", password='" + password + '\'' +
-                '}';
-    }
+
+    @ManyToMany(cascade = CascadeType.ALL)
+    @JoinTable(name = "auth_user_role",
+            joinColumns = @JoinColumn(name = "auth_user_id"),
+            inverseJoinColumns = @JoinColumn(name = "auth_role_id")
+    )private Set<Role> roles;
+
+
 }
