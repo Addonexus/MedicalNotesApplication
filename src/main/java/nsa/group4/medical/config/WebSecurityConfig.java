b6a4daa73608ce6505edf1d22a022d722adae905
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-04 17:16:29 +0000

    web security config file modified again to work with the new method im trying

--- src/main/java/nsa/group4/medical/config/WebSecurityConfig.java
+++ src/main/java/nsa/group4/medical/config/WebSecurityConfig.java
@@ -2,12 +2,14 @@ package nsa.group4.medical.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,89 +18,145 @@ import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig  extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private MyUserDetailsService userDetailsService;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    /**
-     * Set up a password encoder
-     */
+    @Autowired
+    private DataSource dataSource;
 
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    @Value("${spring.queries.users-query}")
+    private String usersQuery;
+
+    @Value("${spring.queries.roles-query}")
+    private String rolesQuery;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().usersByUsernameQuery(usersQuery).authoritiesByUsernameQuery(rolesQuery)
+                .dataSource(dataSource).passwordEncoder(bCryptPasswordEncoder);
     }
-
-//  @Bean
-//  @Override
-//  public UserDetailsService userDetailsService() {
-//    UserDetails user =
-//            User.withDefaultPasswordEncoder()
-//                    .username("user")
-//                    .password("password")
-//                    .roles("USER")
-//                    .build();
-//
-//    return new InMemoryUserDetailsManager(user);
-//  }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        AuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
-
-
-        http
-                .authorizeRequests()
-                .antMatchers("/reports/**").access("hasRole('ROLE_ADMIN')")
-                .antMatchers("/user/**").access("hasRole('ROLE_USER')")
-
-                .antMatchers("/h2_console/**").permitAll()
+        http.authorizeRequests()
+                // URLs matching for access rights
                 .antMatchers("/").permitAll()
-                //.anyRequest().authenticated()
+                .antMatchers("/login").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/home/**").hasAnyAuthority("SUPER_USER", "ADMIN_USER", "SITE_USER")
+                .anyRequest().authenticated()
                 .and()
-                .formLogin()
-                .successHandler(successHandler)
-
+                // form login
+                .csrf().disable().formLogin()
                 .loginPage("/login")
-
-                .permitAll()
+                .failureUrl("/login?error=true")
+                .defaultSuccessUrl("/home")
+                .usernameParameter("email")
+                .passwordParameter("password")
                 .and()
+                // logout
                 .logout()
-                .logoutSuccessUrl("/login")
-                .permitAll()
-                .and()
-                .exceptionHandling().accessDeniedPage("/403")
-                .and()
-                .csrf()
-                .ignoringAntMatchers("/h2-console/**")
-        ;
-
-        http.headers().frameOptions().disable();
-
+                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/").and()
+                .exceptionHandling()
+                .accessDeniedPage("/access-denied");
     }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/resources/**", "/static/**", "/css/**", "/js/**", "/images/**");
+    }
+
+
+
+
+//    @Autowired
+//    private MyUserDetailsService userDetailsService;
+//
+//    /**
+//     * Set up a password encoder
+//     */
+//
+//    @Bean
+//    public static PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//
+////  @Bean
+////  @Override
+////  public UserDetailsService userDetailsService() {
+////    UserDetails user =
+////            User.withDefaultPasswordEncoder()
+////                    .username("user")
+////                    .password("password")
+////                    .roles("USER")
+////                    .build();
+////
+////    return new InMemoryUserDetailsManager(user);
+////  }
+//
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//
+//        AuthenticationSuccessHandler successHandler = new SavedRequestAwareAuthenticationSuccessHandler();
+//
+//
+//        http
+//                .authorizeRequests()
+//                .antMatchers("/reports/**").access("hasRole('ROLE_ADMIN')")
+//                .antMatchers("/user/**").access("hasRole('ROLE_USER')")
+//
+//                .antMatchers("/h2_console/**").permitAll()
+//                .antMatchers("/").permitAll()
+//                //.anyRequest().authenticated()
+//                .and()
+//                .formLogin()
+//                .successHandler(successHandler)
+//
+//                .loginPage("/login")
+//
+//                .permitAll()
+//                .and()
+//                .logout()
+//                .logoutSuccessUrl("/login")
+//                .permitAll()
+//                .and()
+//                .exceptionHandling().accessDeniedPage("/403")
+//                .and()
+//                .csrf()
+//                .ignoringAntMatchers("/h2-console/**")
+//        ;
+//
+//        http.headers().frameOptions().disable();
+//
+//    }
 
     /**
      * Point the authentication to our own user service
      */
 
-    @Bean
-    public DaoAuthenticationProvider authenticationProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService);
-        authProvider.setPasswordEncoder(passwordEncoder());
-        return authProvider;
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.authenticationProvider(authenticationProvider());
-    }
+//    @Bean
+//    public DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+//        authProvider.setUserDetailsService(userDetailsService);
+//        authProvider.setPasswordEncoder(passwordEncoder());
+//        return authProvider;
+//    }
+//
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth.authenticationProvider(authenticationProvider());
+//    }
 //
 //    @Autowired
 //    private UserDetailsService userDetailsService;
