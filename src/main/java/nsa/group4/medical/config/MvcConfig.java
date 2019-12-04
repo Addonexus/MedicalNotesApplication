dcaf328cd065ff708f220ef1cc8b46a5c7b87034
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-04 17:06:21 +0000

    application properities changed and added a bcrypt password encoder in mvcconfig

--- src/main/java/nsa/group4/medical/config/MvcConfig.java
+++ src/main/java/nsa/group4/medical/config/MvcConfig.java
@@ -1,22 +1,31 @@
 package nsa.group4.medical.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+/**
+ * login and registration references from https://www.youtube.com/watch?v=xRE12Y-PFQs&fbclid=IwAR3-l6GCMJPps-ZFHic3QBJOnq6QXwVM3_jt0VLA3H2FfBxH20oIlYjr-H8
+ */
 
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
 
-//        registry.addViewController("/login").setViewName("login");
-//        registry.addViewController("/403").setViewName("403");
-       //registry.addViewController("/templates").setViewName("forward:/templates/");
 
+        //registry.addViewController("/login").setViewName("login");
+        //registry.addViewController("/403").setViewName("403");
+        //registry.addViewController("/templates").setViewName("forward:/templates/");
+        //  registry.addViewController("/reports").setViewName("forward:/reports/hello.html");
 
-        registry.addViewController("/reports").setViewName("forward:/reports/hello.html");
+    }
 
-
-
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+        return bCryptPasswordEncoder;
     }
 }
