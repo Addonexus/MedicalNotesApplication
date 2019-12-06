14515e6e68eb8eb49d19fca9407355c04ec9a66f
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-06 15:51:22 +0000

    user repo added again

--- src/main/java/nsa/group4/medical/service/UserRepository.java
+++ src/main/java/nsa/group4/medical/service/UserRepository.java
@@ -1,8 +1,8 @@
-//package nsa.group4.medical.service;
-//
-//import nsa.group4.medical.domains.User;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserRepository extends JpaRepository<User, Long> {
-//    User findByUsername(String username);
-//}
+package nsa.group4.medical.service;
+
+import nsa.group4.medical.domains.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+}
