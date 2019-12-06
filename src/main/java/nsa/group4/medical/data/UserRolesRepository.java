4fb63f3eb956c67b8c71bdb58cb458321e5cc94c
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-06 15:56:19 +0000

    added new role repo

--- src/main/java/nsa/group4/medical/data/UserRolesRepository.java
+++ src/main/java/nsa/group4/medical/data/UserRolesRepository.java
@@ -1,15 +1,15 @@
-package nsa.group4.medical.data;
-
-
-import nsa.group4.medical.domains.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
-
-public interface UserRolesRepository extends JpaRepository<Role, Long> {
-
-//  @Query("select a.role from Role a, User b where b.username=?1 and a.userid=b.id")
-//  public List<String> findRoleByUsername(String username);
-
-}
+//package nsa.group4.medical.data;
+//
+//
+//import nsa.group4.medical.domains.Role;
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.data.jpa.repository.Query;
+//
+//import java.util.List;
+//
+//public interface UserRolesRepository extends JpaRepository<Role, Long> {
+//
+////  @Query("select a.role from Role a, User b where b.username=?1 and a.userid=b.id")
+////  public List<String> findRoleByUsername(String username);
+//
+//}
