237ad695dd9ef780f9acb29d996c08aa239ac2ae
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-10 13:19:06 +0000

    JPA repo added for ward

---
+++ src/main/java/nsa/group4/medical/data/WardRepositoryJPA.java
@@ -0,0 +1,18 @@
+package nsa.group4.medical.data;
+
+
+import nsa.group4.medical.domains.Diagnosis;
+import nsa.group4.medical.domains.Ward;
+import nsa.group4.medical.service.implementations.WardRepositoryInterface;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WardRepositoryJPA extends WardRepositoryInterface, JpaRepository<Ward, Long> {
+    Optional<Ward> findByName(String name);
+    List<Ward> findByWardNameIn(Collection<String> names);
+}
