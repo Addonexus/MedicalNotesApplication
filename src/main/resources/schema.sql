1a090de9441a9854768180ee8c4688a64681a333
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-10 10:39:23 +0000

    added extra stuff in ward schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -78,6 +78,7 @@ ENGINE = InnoDB;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `ward` (
  `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+ `name` VARCHAR(100) NOT NULL,
   PRIMARY KEY (`id`) )
 ENGINE = InnoDB;
 -- ----------------------------------------------------
