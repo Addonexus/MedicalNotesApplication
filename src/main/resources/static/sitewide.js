ffca72989b1b0a4872a9d355544d526cff6bd0d2
Jake Morris <Jakemorrishello@gmail.com>
2019-12-11 02:25:19 +0000

    styled the logout button

--- src/main/resources/static/sitewide.js
+++ src/main/resources/static/sitewide.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
+$(document).ready(function () {
   $(".sidenav").sidenav();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".modal").modal();
 });
 
-$(document).ready(function() {
+$(document).ready(function () {
   $(".datepicker").datepicker();
 });
 
@@ -19,7 +19,7 @@ function refreshNotifications() {
     notificationList.removeChild(notificationList.firstChild);
   }
 
-  $.get("/api/getAllNotifications", function(data) {
+  $.get("/api/getAllNotifications", function (data) {
     // if (windowUrl.pathname.includes("diagnosis")) {
     //   console.log("diagnosis num: " + lastInt);
     //   for (var i = 0; i < data.length; i++) {
@@ -102,7 +102,7 @@ function refreshNotifications() {
 }
 
 console.log(document.getElementsByClassName("calendar-form"));
-$("#calendar-form").submit(function(e) {
+$("#calendar-form").submit(function (e) {
   e.preventDefault();
   var url = "/api/getCasesByDate/";
 
@@ -119,7 +119,7 @@ $("#calendar-form").submit(function(e) {
     type: "POST",
     url: url,
     data: JSON.stringify(formData),
-    success: function(response) {
+    success: function (response) {
       while (listOfCasesForDay.firstChild) {
         listOfCasesForDay.removeChild(listOfCasesForDay.firstChild);
       }
@@ -136,8 +136,14 @@ $("#calendar-form").submit(function(e) {
         listOfCasesForDay.style.marginBottom = "25px";
       }
     },
-    error: function(json) {
+    error: function (json) {
       alert("Please enter a valid date!");
     }
   });
+});
+
+var form = document.getElementById("logout-form");
+
+document.getElementById("logout").addEventListener("click", function () {
+  form.submit();
 });
