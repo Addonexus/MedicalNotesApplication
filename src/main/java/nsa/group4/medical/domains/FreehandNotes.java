e9a93104154c20d4cec9b88450777affffea13f2
Amina Nessa <NessaAB@cardiff.ac.uk>
2019-12-11 12:45:38 +0000

    added data class for freehand notes

---
+++ src/main/java/nsa/group4/medical/domains/FreehandNotes.java
@@ -0,0 +1,27 @@
+package nsa.group4.medical.domains;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class FreehandNotes {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JsonManagedReference
+    @JoinColumn(name = "diagnosis_id")
+    private Diagnosis diagnosis;
+
+    private String field;
+
+
+}
